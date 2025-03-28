f23ea1c0db1ff01ff60ed5e461e07c451b85d49c
m4niac-0x <m4niac-0x@users.noreply.github.com>
2025-03-28 10:47:28 +0100

    reflactor

--- cmd/fileuploader/fileuploader.go
+++ cmd/fileuploader/fileuploader.go
@@ -25,61 +25,65 @@ const uploadForm = `
 </html>
 `
 
+func renderUploadForm(w http.ResponseWriter) {
+	tmpl, err := template.New("uploadForm").Parse(uploadForm)
+	if err != nil {
+		http.Error(w, "Error rendering form", http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, nil)
+}
+
+func handleFileUpload(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(100 << 20) // 100 MB limit
+	if err != nil {
+		http.Error(w, "Unable to parse form", http.StatusBadRequest)
+		return
+	}
+
+	file, handler, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "Unable to retrieve file", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	dst, err := os.Create(handler.Filename)
+	if err != nil {
+		http.Error(w, "Unable to create file on server", http.StatusInternalServerError)
+		return
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, file)
+	if err != nil {
+		http.Error(w, "Unable to save file", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "File uploaded successfully: %s\n", handler.Filename)
+}
+
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		// Serve the HTML form
-		tmpl := template.New("uploadForm")
-		tmpl, _ = tmpl.Parse(uploadForm)
-		tmpl.Execute(w, nil)
-	} else if r.Method == http.MethodPost {
-		// Parse the multipart form
-		err := r.ParseMultipartForm(100 << 20) // 100 MB limit
-		if err != nil {
-			http.Error(w, "Unable to parse form", http.StatusBadRequest)
-			return
-		}
-
-		// Retrieve the file from the form
-		file, handler, err := r.FormFile("file")
-		if err != nil {
-			http.Error(w, "Unable to retrieve file", http.StatusBadRequest)
-			return
-		}
-		defer file.Close()
-
-		// Create a new file on the server
-		dst, err := os.Create(handler.Filename)
-		if err != nil {
-			http.Error(w, "Unable to create file on server", http.StatusInternalServerError)
-			return
-		}
-		defer dst.Close()
-
-		// Copy the uploaded file to the server
-		_, err = io.Copy(dst, file)
-		if err != nil {
-			http.Error(w, "Unable to save file", http.StatusInternalServerError)
-			return
-		}
-
-		fmt.Fprintf(w, "File uploaded successfully: %s\n", handler.Filename)
-	} else {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		fmt.Fprintln(w, "Only GET and POST methods are allowed")
+	switch r.Method {
+	case http.MethodGet:
+		renderUploadForm(w)
+	case http.MethodPost:
+		handleFileUpload(w, r)
+	default:
+		http.Error(w, "Only GET and POST methods are allowed", http.StatusMethodNotAllowed)
 	}
 }
 
-func StartServer() {
+func startServer() {
 	http.HandleFunc("/", uploadHandler)
 
 	fmt.Println("Starting server on :11337...")
-	err := http.ListenAndServe(":11337", nil)
-	if err != nil {
+	if err := http.ListenAndServe(":11337", nil); err != nil {
 		fmt.Printf("Error starting server: %s\n", err)
 	}
 }
 
 func main() {
-	// Start the server
-	StartServer()
+	startServer()
 }
